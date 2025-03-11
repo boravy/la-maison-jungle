d1ee929b99cf95ada0a6a5954187f203dfebbbcc
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 11:05:49 +0100

    feat(component): care scale component updated with onClick event

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -1,12 +1,31 @@
 import sun from '../assets/sun.svg'
 import water from '../assets/water.svg'
 
+function handleClick({scaleValue, careType}) {
+	let sentence = 'Cette plante requiert '
+	switch (scaleValue) {
+		case 1:
+		  sentence = sentence.concat('peu ')
+		  break;
+		case 2:
+		  sentence = sentence.concat('modérement ')
+		  break;
+		case 3:
+		  sentence = sentence.concat('beaucoup ')
+		  break;
+		default:
+		  sentence = sentence.concat('')
+	}
+	const scaleType = careType === 'light' ? 'de lumière' : 'd\'arrosage'
+	alert(sentence.concat(scaleType))
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType = careType === 'light' ? <img src={sun} alt='sun-icon' /> : <img src={water} alt='water-icon' />
 
 	return (
-		<div>
+		<div onClick={() => handleClick({scaleValue, careType})}>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
