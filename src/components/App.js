fbe9d11bbfbe80f33d7fb1b0bab897f8501293c3
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 13:55:40 +0100

    feat(component): button added for each plant to add it to cart on click

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,24 @@ import logo from '../assets/logo.png'
 
 import '../styles/Layout.css'
 
+import { useState } from 'react'
+
 function App() {
-	return (
-		<div>
-			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
-				<h1 className='lmj-title'>La maison jungle</h1>
-			</Banner>
-			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
-			</div>
-			<Footer />
-		</div>
-	)
+    const [cart, updateCart] = useState([])
+    
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default App
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -2,27 +2,30 @@ import { plantList } from '../datas/plantList'
 import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
-function ShoppingList() {
+function ShoppingList({ cart, updateCart }) {
 	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
 	)
-
+		
 	return (
-		<div>
+		<div className='lmj-shopping-list'>
 			<ul>
-			{categories.map((category) => (
-                    <li key={category}>{ category }</li>
+				{categories.map((cat) => (
+				<li key={cat}>{cat}</li>
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map(({name, cover, id, light, water}) => (
-					<PlantItem key={id} name={name} cover={cover} id={id} light={light} water={water}/>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<div key={id}>
+						<PlantItem cover={cover} name={name} water={water} light={light} />
+						<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
+					</div>
 				))}
 			</ul>
 		</div>
 	)
-}
-
-export default ShoppingList
+	}
+	
+	export default ShoppingList
