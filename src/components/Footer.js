6436bb64c1de6500f57ff1833d4c014a5344b15d
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 11:30:28 +0100

    refactor(component): footer component updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,32 @@
 import { useState } from 'react'
 import '../styles/Footer.css'
 
-function checkMailAddress(value) {
-	if (!value.includes('@')) {
-        alert('Attention, il n\'y a pas d\'@, ceci n\'est pas une adresse valide.')
-    }
-}
-
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
+
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
+
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
 
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
-			<div className='lmj-footer-elem'>Laissez-nous votre mail : 
-                <input type='text' value={inputValue} onChange={(e) => setInputValue(e.target.value)} onBlur={() => checkMailAddress(inputValue)}></input>
-            </div>
+			<div className='lmj-footer-elem'>Laissez-nous votre mail : </div>
+			<input 
+				type='text' 
+				placeholder='Entrez votre mail'
+				value={inputValue} 
+				onChange={(e) => handleInput(e)} 
+				onBlur={(value) => handleBlur(value)}>
+			</input>
 		</footer>
 	)
 }
