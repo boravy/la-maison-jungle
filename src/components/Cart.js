619b7b3a51804577a387545e1ab24944085ec08b
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 14:58:17 +0100

    feat(component): page title updated with total price of cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,18 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-	const total = cart.reduce((acc, plantType) => acc + plantType.amount * plantType.price, 0)
+	const items = Object.keys(cart)
+	const total = items.reduce((acc, item) => acc + cart[item].amount * cart[item].price, 0)
+	
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
     
 	return isOpen ? (
 		<div className='lmj-cart'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(false)}
-			>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>
 				Fermer
 			</button>
 			{cart.length > 0 ? (
@@ -23,19 +25,15 @@ function Cart({ cart, updateCart }) {
 							</div>
 						))}
 					</ul>
-					<h3>Total :{total}€</h3>
+					<h3>Total : {total}€</h3>
 					<button onClick={() => updateCart([])}>Vider le panier</button>
 				</div>
-			) : (
-				<div>Votre panier est vide</div>
-			)}
+			) : 
+			<div>Votre panier est vide</div>}
 		</div>
 	) : (
 		<div className='lmj-cart-closed'>
-			<button
-				className='lmj-cart-toggle-button'
-				onClick={() => setIsOpen(true)}
-			>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>
 				Ouvrir le Panier
 			</button>
 		</div>
