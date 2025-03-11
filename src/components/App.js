a159ad96854d2fdf801a9faa518bd8391cec7ebf
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 09:31:57 +0100

    feat(component): banner component updated with children props

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import Banner from './Banner'
 import ShoppingList from './ShoppingList'
+import logo from '../assets/logo.png'
 
 function App() {
-    return <div>
-            <Banner />
-            {/* <Cart /> */}
-            <ShoppingList />
-        </div>
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
 export default App
